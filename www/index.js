599a6a5d81bbb96e22f5e33a74b9c6717764bfbd
Brandon Martel <brandonmartel@gmail.com>
2023-07-30 10:03:23 -0500

    include free rights samples

--- www/index.js
+++ www/index.js
@@ -1,22 +1,28 @@
 import { memory } from "pulsar/pulsar_bg.wasm";
 import { AudioDecoder } from "pulsar";
 
-// const decoder = AudioDecoder.new(
-// );
+const sampleOgg =
+  "https://raw.githubusercontent.com/bmartel/pulsar/main/www/assets/sample_10MB_OGG.ogg";
+const sampleMp3 =
+  "https://raw.githubusercontent.com/bmartel/pulsar/main/www/assets/sample_1OMB_MP3.mp3";
+const sampleWav =
+  "https://raw.githubusercontent.com/bmartel/pulsar/main/www/assets/sample_10MB_WAV.wav";
 
-// decoder
-//   .decode()
-//   .then(() => {
-//     const err = decoder.get_last_error();
-//     console.log(err);
-//     const samplesPtr = decoder.get_samples();
-//     const samples = new Float32Array(
-//       memory.buffer,
-//       samplesPtr,
-//       decoder.get_samples_len()
-//     );
-//     console.log(samples);
-//   })
-//   .catch((err) => {
-//     console.error(JSON.parse(err));
-//   });
+const decoder = AudioDecoder.new(sampleOgg);
+
+decoder
+  .decode()
+  .then(() => {
+    const err = decoder.get_last_error();
+    console.log(err);
+    const samplesPtr = decoder.get_samples();
+    const samples = new Float32Array(
+      memory.buffer,
+      samplesPtr,
+      decoder.get_samples_len()
+    );
+    console.log(samples);
+  })
+  .catch((err) => {
+    console.error(JSON.parse(err));
+  });
